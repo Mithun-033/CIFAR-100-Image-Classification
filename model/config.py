bcca731fe2dd49e922805da4d18dc2887b52509c
PANKAJ SHARMA <pankajofficial2507ps@gmail.com>
2026-07-09 18:05:55 +0530

    CNN architecture defined and set param's

--- model/config.py
+++ model/config.py
@@ -3,18 +3,27 @@ from dataclasses import dataclass
 @dataclass
 class model_config:
     num_classes: int = 100
-    conv1_out_channels: int = ...
-    conv2_out_channels: int = ...
-    conv3_out_channels: int = ...
-    ...
+    conv1_out_channels: int = 32
+    conv2_out_channels: int = 64
+    kernel_size: int = 3
+
+
+    pool_stride :int = 2
+    pool_kernel :int = 2
+
+    input_features:int = 4096 #64*8*8
+    hidden_features1: int = 1028
+    hidden_features2: int = 512
+
+    dropout_rate : float = 0.3
+    
 
 @dataclass
 class training_config:
     # dataloader parameters
-    batch_size: int = ...
-    num_workers: int = ...
+    batch_size: int = 250
+    num_workers: int = 4
     # training parameters
-    learning_rate: float = ...
-    num_epochs: int = ...
-    weight_decay: float = ...
-    ...
+    learning_rate: float = 1e-2
+    num_epochs: int = 100
+    weight_decay: float = 1e-4
--- model/model.py
+++ model/model.py
@@ -1,18 +1,47 @@
-''' Placeholder for the model architecture. '''
-
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from model.config import model_config
+from config import model_config
 
 class CIFAR100Model(nn.Module):
     def __init__(self, config):
-        ''' Initialize the model. '''
+        
         super().__init__()
 
-        ...
-        ...
-        ...
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3,
+                      out_channels=config.conv1_out_channels,
+                      kernel_size=config.kernel_size,
+                      padding='same'
+                      ),
+            nn.ReLU(),
+            nn.BatchNorm2d(config.conv1_out_channels),
+            nn.MaxPool2d(kernel_size = config.pool_kernel, stride = config.pool_stride), 
 
+            nn.Conv2d(in_channels=config.conv1_out_channels,
+                      out_channels=config.conv2_out_channels,
+                      kernel_size=config.kernel_size,
+                      padding='same'
+                      ),
+            nn.ReLU(),
+            nn.BatchNorm2d(config.conv2_out_channels),
+            nn.MaxPool2d(kernel_size =config.pool_kernel, stride =config.pool_stride)
+            )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(config.input_features , 
+                    config.hidden_features1),
+            nn.ReLU(),
+            nn.Dropout(p=config.dropout_rate),
+
+
+            nn.Linear(config.hidden_features1,
+                    config.hidden_features2),
+            nn.ReLU(),
+            nn.Dropout(p=config.dropout_rate),
+
+            nn.Linear(config.hidden_features2 , config.num_classes)
+        )
         self.apply(self._init_weights)
 
     def _init_weights(self,module):
@@ -22,10 +51,9 @@ class CIFAR100Model(nn.Module):
         ...
 
     def forward(self, x):
-        ''' Forward pass of the model. '''
-
-        ...
-        ...
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
 
 if __name__ == "__main__":
     model = CIFAR100Model(config = model_config())
